00abb4545a06359de9a9b899968d485c71602f91
unknown <KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.ru>
2020-10-06 14:32:21 +0300

    completed task towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if(matrix === undefined) return []
+  let sort = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0 || i === 0) {
+      console.log('-')
+
+      for (let j = 0; j < matrix[i].length; j++) {
+        sort.push(matrix[i][j])
+      }
+
+    } else {
+      matrix[i] = matrix[i].reverse();
+      for (let j = 0; j < matrix[i].length; j++) {
+        sort.push(matrix[i][j])
+      }
+    }
+  }
+
+  return sort
 }
